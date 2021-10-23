acc6a699b7e4016e64bef372077e1727a4f6e93d
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-23 11:20:47 +0700

    Create the project

---
+++ SB-PhoneAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB-PhoneAuth
+//
+//  Created by Danh Tu on 23/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
